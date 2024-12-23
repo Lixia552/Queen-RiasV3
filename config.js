7df3b08abb3c31cae4d6451a630224ace5d55894
Lixia552 <dosunmubisola@gmail.com>
2024-12-23 14:59:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ require('dotenv').config(); // Load environment variables
 module.exports = {
     prefix: process.env.PREFIX || '.', // Command prefix
     ownerName: process.env.OWNER_NAME || 'Toxxic', // Owner name
-    ownerNumber: process.env.OWNER_NUMBER || '2348165846414', // Your WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER || '2349032040887', // Your WhatsApp number
     mode: process.env.MODE || 'private', // Bot mode: 'public' or 'private'
     region: process.env.REGION || 'Nigeria', // Region
     botName: process.env.BOT_NAME || 'Rias Gremory V3', // Bot name
-    exifPack: process.env.EXIF_PACK || 'RIAS V3 LOVES', // Sticker pack name
-    exifAuthor: process.env.EXIF_AUTHOR || 'Toxxic', // Author of the sticker pack
+    exifPack: process.env.EXIF_PACK || 'Rikka†', // Sticker pack name
+    exifAuthor: process.env.EXIF_AUTHOR || 'Rikka', // Author of the sticker pack
     timeZone: process.env.TIME_ZONE || 'Africa/Lagos', // Time zone
-    presenceStatus: process.env.PRESENCE_STATUS || 'recording', // Bot presence status
+    presenceStatus: process.env.PRESENCE_STATUS || 'typing', // Bot presence status
     autoRead: process.env.AUTO_READ === 'true', // Auto-read messages (true or false)
-    autoViewStatus: process.env.AUTO_VIEW_STATUS === 'true', // Auto-view statuses (true or false)
+    autoViewStatus: process.env.AUTO_VIEW_STATUS === 'false', // Auto-view statuses (true or false)
     sessionId: process.env.SESSION_ID || '', // WhatsApp session ID
 };
