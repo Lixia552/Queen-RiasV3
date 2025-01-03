7a971055cd831a4518d7b7ebc9d2d689ac840470
Lixia552 <dosunmubisola@gmail.com>
2025-01-03 15:03:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load environment variables
 module.exports = {
     prefix: process.env.PREFIX || '.', // Command prefix
     ownerName: process.env.OWNER_NAME || 'Toxxic', // Owner name
-    ownerNumber: process.env.OWNER_NUMBER || '2349032040887', // Your WhatsApp number
+    ownerNumber: process.env.OWNER_NUMBER || '2349152662959', // Your WhatsApp number
     mode: process.env.MODE || 'private', // Bot mode: 'public' or 'private'
     region: process.env.REGION || 'Nigeria', // Region
     botName: process.env.BOT_NAME || 'Rias Gremory V3', // Bot name
@@ -11,7 +11,7 @@ module.exports = {
     exifAuthor: process.env.EXIF_AUTHOR || 'Rikka', // Author of the sticker pack
     timeZone: process.env.TIME_ZONE || 'Africa/Lagos', // Time zone
     presenceStatus: process.env.PRESENCE_STATUS || 'typing', // Bot presence status
-    autoRead: process.env.AUTO_READ === 'true', // Auto-read messages (true or false)
+    autoRead: process.env.AUTO_READ === 'false', // Auto-read messages (true or false)
     autoViewStatus: process.env.AUTO_VIEW_STATUS === 'false', // Auto-view statuses (true or false)
     sessionId: process.env.SESSION_ID || '', // WhatsApp session ID
 };
